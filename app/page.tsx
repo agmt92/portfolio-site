0699c3d07312e8e8a7941b814cc2a12dd1878e88
agmt92 <155798390+agmt92@users.noreply.github.com>
2024-09-05 11:45:23 +0200

    feat: create layout.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import VideoBackground from './ui/landing-page/VideoBackground';
 import PassportSection from './ui/landing-page/PassportSection';
-import Navbar from './ui/landing-page/navbar-top';
-import Footer from './ui/landing-page/footer';
 import LogosConveyer from './ui/landing-page/logos-conveyer';
 
 export default function Home() {
   return (
     <main className="flex flex-col">
-      <Navbar />
       <VideoBackground />
       <PassportSection />
       <LogosConveyer />
-      <Footer />
     </main>
   );
 }
