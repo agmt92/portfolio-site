a7a0a6b2bb3a8e13ccd126eed546fd69f3f0f29a
agmt92 <155798390+agmt92@users.noreply.github.com>
2024-09-08 12:09:28 +0200

    feat: create contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Form from "../ui/contact-section/contact";
+
 export default function Page() {
     return (
       <main className="flex flex-col">
           <span className="text-4xl font-bold">Contact Section</span>
+          <Form />
       </main>
     );
   }
